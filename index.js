af05270f6467abe9e8e2190d3f453be36f6b3092
kelly <kelly.n.waite@gmail.com>
2017-01-13 10:26:48 -0700

    Make arr into new arr w.out articles, Sort that arr alphabetically

---
+++ index.js
@@ -0,0 +1,35 @@
+const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
+
+const compare = (test, b) => {
+    if ( test < b) {
+      return -1
+    }  else if ( test > b) {
+      return 1
+    }
+    else return 0;
+}
+
+const takeOutArticles = (c) => {
+  let test;
+  const artThe = "The",
+          artAn = "An",
+          artA = "A";
+  if ( c.includes(artThe) ) {
+    test = c.replace('The ', '')
+  } else if ( c.includes(artAn) ) {
+    test = c.replace('An ', '');
+  } else if ( c.includes(artA) ) {
+      test = c.replace('A ', '')
+  } else {
+    test = c
+  }
+  return test
+}
+
+const newArr = bands.map(takeOutArticles)
+
+console.log(newArr);
+
+const sorted = newArr.sort();
+
+console.log(sorted);
