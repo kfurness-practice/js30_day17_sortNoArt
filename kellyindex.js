8321c2a596ddc10a78b60f8960e788e8742807bc
kelly <kelly.n.waite@gmail.com>
2017-01-13 11:29:33 -0700

    Clean up code with help of Wes Bos
    Use more ES6; Include function in if statement or sort

--- kellyindex.js
+++ kellyindex.js
@@ -1,15 +1,6 @@
 const bandsList = document.querySelector('#bands');
 const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
 
-const compare = (test, b) => {
-    if ( test < b) {
-      return -1
-    }  else if ( test > b) {
-      return 1
-    }
-    else return 0;
-}
-
 const takeOutArticles = (c) => {
   let test;
   const artThe = "The",
@@ -34,7 +25,3 @@ sorted.forEach( (c) => {
     const listItem =  `<li>${c}</li>`;
     bandsList.innerHTML += listItem;
  })
-
-
-
-console.log(sorted);
